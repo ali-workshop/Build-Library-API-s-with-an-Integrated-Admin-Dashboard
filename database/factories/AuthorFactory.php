4c4f3d3125407e62c66ac63084e1476946fa59ac
ALI ALI <alirafikali22@gmail.com>
2024-05-07 10:18:37 +0300

    fix bugs and reinstall the author model

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\author>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
  */
 class AuthorFactory extends Factory
 {
