aac573eb167bf8c87a4caabe12c46d95e38f643c
ALI ALI <alirafikali22@gmail.com>
2024-05-07 10:14:00 +0300

    build the author model

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\author;
+use App\Http\Requests\StoreauthorRequest;
+use App\Http\Requests\UpdateauthorRequest;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreauthorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(author $author)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateauthorRequest $request, author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(author $author)
+    {
+        //
+    }
+}
