5961536250eb20affcccacd6a7dd863482e87560
ALI ALI <alirafikali22@gmail.com>
2024-05-08 03:53:08 +0300

    fix bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.base>
     {{-- If the user is authenticated --}}
-    @auth()
+    @auth
         {{-- If the user is authenticated on the static sign up or the sign up page --}}
         @if (in_array(request()->route()->getName(),['static-sign-up', 'sign-up'],))
             @include('layouts.navbars.guest.sign-up')
