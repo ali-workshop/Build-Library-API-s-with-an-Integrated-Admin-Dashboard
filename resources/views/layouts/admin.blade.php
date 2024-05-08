a2ada942160bb6830a17aa1762fec9b81392caf2
ALI ALI <alirafikali22@gmail.com>
2024-05-08 06:18:32 +0300

    add layout for livewire

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- head content -->
+    </head>
+    <body>
+        <div class="container">
+            <!-- Other content -->
+
+            @yield('content') 
+            <!-- Other content -->
+        </div>
+
+        <!-- scripts -->
+    </body>
+</html>
