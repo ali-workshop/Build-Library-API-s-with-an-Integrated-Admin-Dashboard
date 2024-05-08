928dd75d516543d4dcb24e43cedbd0cec631b5ed
ALI ALI <alirafikali22@gmail.com>
2024-05-08 07:08:17 +0300

    add the class which represent the controllers

--- app/Http/Livewire/AddSubCategory.php
+++ app/Http/Livewire/AddSubCategory.php
@@ -3,9 +3,43 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\SubCategory;
+use PHPUnit\Framework\Exception;
 
 class AddSubCategory extends Component
 {
+    
+
+    public $sub_cat_name = '';
+    public $cat_id =0 ;
+    
+
+    
+    
+    public function saveSubCategory(){
+        
+                $this->validate([
+                    'sub_cat_name' => 'required',
+                    'cat_id' => ['required',]#'exists:categori,column'],
+                ]);
+                try {
+                    $new_sub_cat = new SubCategory;
+                    $new_sub_cat->name = $this->sub_cat_name;
+                    $new_sub_cat->category_id = $this->cat_id;
+                    $new_sub_cat->save();
+                    // ,navigate:true for the SPA
+                    return $this->redirect('/'); 
+                    // add the last parameter it's important for not reloading the whole page..
+                    // and give us the way to achieve the SPA..
+                } catch (Exception $e) {
+                    dd($e) ;
+                }
+    
+    
+    
+    
+            }
+   
     public function render()
     {
         return view('livewire.add-sub-category');
--- app/Http/Livewire/StoreBook.php
+++ app/Http/Livewire/StoreBook.php
@@ -2,10 +2,67 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Book;
+use App\Models\Author;
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\SubCategory;
+use PHPUnit\Framework\Exception;
 
 class StoreBook extends Component
 {
+
+    public $title = '';
+    public $favorite = false;
+    public $publicationDate ='';
+    public $src = '';
+
+    public $briefDescription = '';
+    public $category_name = '';
+    public $subcategory_name = '';
+    public $author_name = '';
+    public $biography = '';
+
+public function createBook(){
+
+
+    $this->validate([
+        'title' => 'required',
+        'favorite' => 'required',
+        'publicationDate' => 'required',
+        'src' => 'required',
+        'briefDescription' => 'required',
+        'category_name' => 'required',
+        'subcategory_name' => 'required',
+        'author_name' => 'required',
+        'biography' => 'required',
+    ]);
+    $category = Category::firstOrCreate(['name' => $this->category_name]);
+    $subcategory = SubCategory::firstOrCreate(['name' => $this->subcategory_name], ['category_id' => $category->id]);
+    $author = Author::firstOrCreate(['name' => $this->author_name], ['biography' =>$this->biography ]);
+    
+    try{
+        $book=new Book;
+        $book->title=$this->title;
+        $book->favorite=$this->favorite;
+        $book->publicationDate=$this->publicationDate;
+        $book->src=$this->src;
+        $book->briefDescription=$this->briefDescription;
+        $book->category_id=$category->id;
+        $book->subcategory_id=$subcategory->id;
+        $book->author_id=$author->id;
+        $book->rate_id=1;
+        $book->save();
+
+        // ,navigate:true
+        return $this->redirect('/');
+
+    } catch (Exception $e) {
+        dd($e) ;
+    }
+    
+}
+
     public function render()
     {
         return view('livewire.store-book');
